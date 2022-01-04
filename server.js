6b11bb0c816ecb7c2ac6cc46b03590e8d0c96cbd
rob49157 <robertopaipo@hotmail.com>
2022-01-03 21:52:13 -0500

    debuggin server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const mongodb= require('mongodb').MongoClient
+// const express = require('express')
+// const mongodb= require('mongodb').MongoClient
 
-const app= express()
-const port = 3001
+// const app= express()
+// const port = 3001
+
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/socialmediadb", ()=>{
+    console.log('connected')
+})
+
