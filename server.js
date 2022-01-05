317d6833be2af3c082988dd5686529dda6b6cd7b
rob49157 <robertopaipo@hotmail.com>
2022-01-04 21:17:26 -0500

    npm run devStart

--- server.js
+++ server.js
@@ -5,8 +5,14 @@
 // const port = 3001
 
 const mongoose = require("mongoose")
+const User= require("./schema")
 
-mongoose.connect("mongodb://localhost/socialmediadb", ()=>{
-    console.log('connected')
-})
 
+mongoose.connect("mongodb://localhost/socialmediadb")
+run()
+async function run(){
+    const user =await User.create({name:"roberto", age:26})
+    user.name= "andros"
+    await user.save()
+    console.log(user)
+}
